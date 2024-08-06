bbd96f258b9cdad58b73660d7b3ecf53b0ac372b
Vicente Cartas <vicente.cartas@outlook.com>
2024-08-05 23:04:37 -0700

    Math for hex bouding box

--- src/tiled/tilEdSpriteMap.ts
+++ src/tiled/tilEdSpriteMap.ts
@@ -5,13 +5,13 @@ export class tilEdSpriteMap {
     private _map: TilEdMap;
     private _mapPixelSize: Vector2;
     private _scene: Scene;
-    private _tilesTexture: RawTexture;
+    private _mapTexture: RawTexture;
 
     constructor(map: TilEdMap, scene: Scene) {
         this._map = map;
         this._mapPixelSize = this._calculateMapPixelSize(map);
         this._scene = scene;
-        this._tilesTexture = RawTexture.CreateRGBATexture(
+        this._mapTexture = RawTexture.CreateRGBATexture(
             new Uint8Array(this._mapPixelSize.x * this._mapPixelSize.y * 4),
             this._mapPixelSize.x,
             this._mapPixelSize.y,
@@ -21,27 +21,32 @@ export class tilEdSpriteMap {
             Texture.NEAREST_NEAREST,
             Engine.TEXTURETYPE_FLOAT);
         
-        this._updateTexture(this._map, this._tilesTexture);
+        this._updateMapTexture(this._map, this._mapTexture);
     }
 
     private _calculateMapPixelSize(map: TilEdMap) : Vector2 {
         if (map.orientation === 'orthogonal') {
             return new Vector2(map.width * map.tileWidth, map.height * map.tileHeight);
-        } else if (map.orientation === 'hexagonal') {
+        } else if (map.orientation === 'hexagonal' && map.hexSideLength) {
             if (map.staggerAxis === 'x') {
-                return new Vector2(0, 0);
+                return new Vector2(
+                    Math.ceil(map.width / 2) * map.tileWidth + Math.floor(map.width / 2) * map.hexSideLength,
+                    map.height * map.tileHeight + map.tileHeight / 2);
             } else {
-                return new Vector2(0, 0);
+                return new Vector2(
+                    map.width * map.tileWidth + map.tileWidth / 2,
+                    Math.ceil(map.height / 2) * map.tileHeight + Math.floor(map.height / 2) * map.hexSideLength,
+                    );
             }
         } else if (map.orientation === 'isometric') {
-            return new Vector2(0, 0);
+            throw new Error('Not implemented');
         }
 
         throw new Error('Invalid map orientation');
     }
 
     // Renders the TilEd map into the raw texture
-    private _updateTexture(map: TilEdMap, tilesTexture: RawTexture) {
+    private _updateMapTexture(map: TilEdMap, tilesTexture: RawTexture) {
         
     }
 }
