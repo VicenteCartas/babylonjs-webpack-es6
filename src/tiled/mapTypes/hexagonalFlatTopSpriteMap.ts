ebdf7d15865c8b4e80bce3bba65ef1d7422fad4d
Vicente Cartas <vicente.cartas@outlook.com>
2024-08-06 23:58:55 -0700

    Add code for orthogonal map rendering

---
+++ src/tiled/mapTypes/hexagonalFlatTopSpriteMap.ts
@@ -0,0 +1,23 @@
+import { Engine, RawTexture, Scene, Texture, Vector, Vector2 } from "@babylonjs/core";
+import { ITilEdSpriteMap, TilEdMap } from "../tilEd.types";
+
+export class HexagonalFlatTopSpriteMap implements ITilEdSpriteMap {
+    private _map: TilEdMap;
+    private _scene: Scene;
+
+    constructor(map: TilEdMap, scene: Scene) {
+        this._map = map;
+        this._scene = scene;
+    }
+
+    public getMapPixelSize(): Vector2 {
+        if (this._map.hexSideLength) {
+            return new Vector2(
+                Math.ceil(this._map.width / 2) * this._map.tileWidth + Math.floor(this._map.width / 2) * this._map.hexSideLength,
+                this._map.height * this._map.tileHeight + this._map.tileHeight / 2);
+    
+        }
+
+        throw new Error('HexSideLenght not defined for hexagonal map');
+    }
+}
---
+++ src/tiled/mapTypes/hexagonalPointTopSpriteMap.ts
@@ -0,0 +1,24 @@
+import { Engine, RawTexture, Scene, Texture, Vector, Vector2 } from "@babylonjs/core";
+import { ITilEdSpriteMap, TilEdMap } from "../tilEd.types";
+
+export class HexagonalPointTopSpriteMap implements ITilEdSpriteMap {
+    private _map: TilEdMap;
+    private _scene: Scene;
+
+    constructor(map: TilEdMap, scene: Scene) {
+        this._map = map;
+        this._scene = scene;
+    }
+
+    public getMapPixelSize(): Vector2 {
+        if (this._map.hexSideLength) {
+            return new Vector2(
+                this._map.width * this._map.tileWidth + this._map.tileWidth / 2,
+                Math.ceil(this._map.height / 2) * this._map.tileHeight + Math.floor(this._map.height / 2) * this._map.hexSideLength
+            );
+    
+        }
+
+        throw new Error('HexSideLenght not defined for hexagonal map');
+    }
+}
