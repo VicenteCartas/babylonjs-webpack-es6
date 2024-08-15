4604da21bc44c6176199f5d12edf9886c99c2c58
Vicente Cartas <vicente.cartas@outlook.com>
2024-08-14 23:25:40 -0700

    Fixing last orthogonal math bugs, rendering in a plane

--- src/scenes/tiledScene.ts
+++ src/scenes/tiledScene.ts
@@ -1,8 +1,8 @@
 import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
 import { PointLight } from "@babylonjs/core/Lights/pointLight";
 import { Scene } from "@babylonjs/core/scene";
-import { Vector3 } from "@babylonjs/core/Maths/math.vector";
-import { Color3, Color4, FreeCamera, HemisphericLight, MeshBuilder, StandardMaterial, Texture } from "@babylonjs/core";
+import { Vector3, Vector4 } from "@babylonjs/core/Maths/math.vector";
+import { Color3, Color4, FreeCamera, HemisphericLight, Mesh, MeshBuilder, StandardMaterial, Texture } from "@babylonjs/core";
 import { AbstractEngine } from "@babylonjs/core/Engines/abstractEngine";
 
 import "@babylonjs/core/Lights/Shadows/shadowGeneratorSceneComponent";
@@ -32,45 +32,43 @@ export class TilEdScene implements CreateSceneClass {
             });
         });
 
-        const camera = new ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 8, Vector3.Zero());
-        camera.position = new Vector3(camera.position.x, camera.position.y + 100, camera.position.z - 100);
+        const camera = new ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 3, Vector3.Zero());
+        camera.attachControl(canvas, true);
+        const light = new HemisphericLight("light", new Vector3(1, 1, 0));
+        light.intensity = 10;
+
+        //const camera = new ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 8, Vector3.Zero());
+        //camera.position = new Vector3(camera.position.x, camera.position.y + 100, camera.position.z - 100);
         //camera.attachControl(canvas, true);
-        const light = new PointLight("Point", new Vector3(5, 10, 5), scene);
+        //const light = new PointLight("Point", new Vector3(5, 10, 5), scene);
         //light.intensity = 0.7;
 
-        //const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-        //camera.setTarget(Vector3.Zero());
-        camera.attachControl(canvas, true);
-        //const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-        light.intensity = 0.7;
-
         const importer = new TilEdImporter();
-        const map = await TilEdImporter.ImportMapAsync('http://localhost:8080/maps/NewRenderingMap.tmx', scene);
+        //const map = await TilEdImporter.ImportMapAsync('http://localhost:8080/maps/NewRenderingMap.tmx', scene);
         //const map = await TilEdImporter.ImportMapAsync('http://localhost:8080/maps/cityMap.tmx', scene);
-        //const map = await TilEdImporter.ImportMapAsync('http://localhost:8080/maps/worldMap.tmx', scene);
+        const map = await TilEdImporter.ImportMapAsync('http://localhost:8080/maps/worldMap.tmx', scene);
         const spriteMap = getTiledSpriteMap(map, scene);
         const size = spriteMap.getMapPixelSize();
         const groundTexture = await spriteMap.renderToTexture();
 
-        // Our built-in 'ground' shape.
-        var ground = MeshBuilder.CreateGround("ground", {width: size.x, height: size.y}, scene);
-        let groundMaterial = new StandardMaterial("Ground Material", scene);
-        //let groundTexture = new Texture('http://localhost:8080/maps/SimpleImage.png', scene);
-        //groundMaterial.diffuseColor = Color3.Red();
-        groundMaterial.diffuseTexture = groundTexture;
-        ground.material = groundMaterial;
+        const mat = new StandardMaterial("");
+	    mat.diffuseTexture = groundTexture;
+
+        const f = new Vector4(0, 0, 1, 1); // front image = half the whole image along the width 
+        const b = new Vector4(0, 0, 1, 1); // back image = second half along the width
+        
+        const plane = MeshBuilder.CreatePlane("plane", { width: size.x, height: size.y, frontUVs: f, backUVs: b, sideOrientation: Mesh.FRONTSIDE});
+        plane.material = mat;
+
+        // // Our built-in 'ground' shape.
+        // var ground = MeshBuilder.CreateGround("ground", {width: size.x, height: size.y}, scene);
+        // let groundMaterial = new StandardMaterial("Ground Material", scene);
+        // //let groundTexture = new Texture('http://localhost:8080/maps/SimpleImage.png', scene);
+        // //groundMaterial.diffuseColor = Color3.Red();
+        // groundMaterial.diffuseTexture = groundTexture;
+        // ground.material = groundMaterial;
 
 
-
-        //const map = await TilEdImporter.ImportMapAsync('http://localhost:8080/maps/cityMap.tmx', scene);
-        //const map = await TilEdImporter.ImportMapAsync('http://localhost:8080/maps/worldMap.tmx', scene);
-        //const map = await TilEdImporter.ImportMapAsync('http://localhost:8080/maps/HexagonalMap.tmx', scene);
-
-        //await TilEdRenderer.DebugTileset(map.tilesets[0], scene);
-        //await TilEdRenderer.RenderTilemap(map, scene);
-
-        //await TilEdRenderer.DebugTileset(map.tilesets[0], scene);
-        //await TilEdRenderer.RenderTilemap(map, scene);
         
         return scene;
     };
